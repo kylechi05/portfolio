cd1b44fdefd2b0b6445ed73012f5e097b30f096d
Kyle Chi <kylechi2157@gmail.com>
2025-08-20 17:29:31 -0500

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,20 +18,10 @@ export default function AboutPage() {
                     <HeadshotContainer src="/kyle_photo.jpg" />
                     <p>
                         Hi, I&apos;m Kyle Chi, a computer science major and
-                        mathematics minor at the University of Iowa. I have
-                        experience in full-stack development and a growing
-                        interest in system design, DevOps, and machine learning.
-                        Currently, I&apos;m a Software Development Intern at{" "}
-                        <a
-                            href="https://www.berkley.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="underline"
-                        >
-                            W.R. Berkley
-                        </a>
-                        , and I will begin my final two semesters at UIowa this
-                        Fall.
+                        mathematics minor at the University of Iowa, graduating
+                        in two semesters. I have experience in full-stack
+                        development and a growing interest in system design,
+                        DevOps, and machine learning.
                     </p>
                 </div>
             </div>
